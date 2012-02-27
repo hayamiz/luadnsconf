5aa3c5a3cd55c22a52cfbf6073ef3eb798688bd3
Yuto Hayamizu <y.hayamizu@gmail.com>
2012-02-27 21:25:20 +0900

    added mx and cname records

--- hayamiz.com.lua
+++ hayamiz.com.lua
@@ -3,4 +3,11 @@
 -- _a = "hayamiz.com"
 
 a(_a, "59.106.186.140")
-a("www.".._a, "59.106.186.140")
+cname("www", _a)
+cname("volvox", _a)
+
+a(concat("mailhost", _a),	"59.106.186.140")
+a(concat("lists", _a),		"59.106.186.140")
+
+mx(_a, concat("mailhost", _a), 10)
+mx(concat("lists", _a), concat("mailhost", _a), 10)
