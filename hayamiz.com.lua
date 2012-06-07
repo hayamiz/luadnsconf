4d68c55a36896d72caa0a6d2e36fb13b2402caee
Yuto Hayamizu <y.hayamizu@gmail.com>
2012-06-08 02:14:42 +0900

    add jenkins subdomain

--- hayamiz.com.lua
+++ hayamiz.com.lua
@@ -9,7 +9,7 @@ ipaddr = "49.212.150.25"
 -- configurations
 a(_a, ipaddr, ttl)
 cname("www", _a, ttl)
-cname("volvox", _a, ttl)
+cname("jenkins", _a, ttl)
 
 a(concat("mail", _a),	ipaddr, ttl)
 a(concat("mailhost", _a),	ipaddr, ttl)
