180059987be6fca72953e79712c8a7bda8efde28
Yuto Hayamizu <y.hayamizu@gmail.com>
2012-08-14 23:30:55 +0900

    add domain: supico.info

---
+++ supico.info.lua
@@ -0,0 +1,19 @@
+-- File: supico.info.lua
+-- Variable _a is replaced with zone name by Luadns
+-- _a = "supico.info"
+
+-- parameters
+ttl = 3600
+ipaddr = "49.212.150.25"
+
+-- configurations
+a(_a, ipaddr, ttl)
+cname("www", _a, ttl)
+
+a(concat("mail", _a),	ipaddr, ttl)
+a(concat("mailhost", _a),	ipaddr, ttl)
+a(concat("lists", _a),		ipaddr, ttl)
+
+mx(_a, concat("mailhost", _a), 10, ttl)
+txt(_a, "v=spf1 a mx ptr ip4:" .. ipaddr .. " ptr:mailhost." .. _a .. " mx:mailhost." .. _a .. " mx:" .. _a .. " -all", ttl)
+txt(concat("lists", _a), "v=spf1 a mx ptr ip4:" .. ipaddr .. " ptr:mailhost." .. _a .. " mx:mailhost." .. _a .. " mx:" .. _a .. " -all", ttl)
