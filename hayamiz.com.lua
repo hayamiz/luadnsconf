ebd633808397f69d13bc385340993723dc0cb992
Yuto Hayamizu <y.hayamizu@gmail.com>
2012-08-14 23:33:08 +0900

    removed testv4/v6 records

--- hayamiz.com.lua
+++ hayamiz.com.lua
@@ -15,10 +15,6 @@ a(concat("mail", _a),	ipaddr, ttl)
 a(concat("mailhost", _a),	ipaddr, ttl)
 a(concat("lists", _a),		ipaddr, ttl)
 
--- IPv6 test
-a(concat("testv4", _a),	"59.106.186.140", ttl)
-aaaa(concat("testv6", _a),	"2001:e41:3b6a:ba8c::1", ttl)
-
 mx(_a, concat("mailhost", _a), 10, ttl)
 mx(concat("lists", _a), concat("mailhost", _a), 10, ttl)
 txt(_a, "v=spf1 a mx ptr ip4:" .. ipaddr .. " ptr:mailhost." .. _a .. " mx:mailhost." .. _a .. " mx:" .. _a .. " -all", ttl)
